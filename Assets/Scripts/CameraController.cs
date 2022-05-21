105f154999dce5a222cd7b8cddd7214f800b2a63
Michael Montagnese <mmont429@gmail.com>
2022-05-21 23:00:04 +0100

    Update camera script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = target.transform.position + new Vector3(8, 4, -1);
+        transform.LookAt(target);
     }
 }
