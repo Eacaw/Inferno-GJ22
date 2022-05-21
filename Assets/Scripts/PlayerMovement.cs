e93b7a32a134b3d8e1bebcecf2eff094dfcf322b
Michael Montagnese <mmont429@gmail.com>
2022-05-21 23:52:55 +0100

    Add jumping to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     // Base user speed
     [SerializeField] private float speed = 5.0f;
+    private Rigidbody rb;
+
+    public int MaxJump = 1;
+    int JumpCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,6 +22,25 @@ public class PlayerMovement : MonoBehaviour
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && JumpCount < MaxJump)
+        {
+            Jump();
+        }
+
         transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        JumpCount += 1;
+    }
+
+    void OnCollisionEnter(Collision Col)
+    {
+        if (Col.gameObject.tag == "Ground")
+        {
+            JumpCount = 0;
+        }
     }
 }
