810a5cac3dae6d8771abe5dee079b3049e2b7c3a
Eacaw <davidpinchen@gmail.com>
2022-05-22 16:42:13 +0100

    Add trigger interface method body

--- Assets/Scripts/PrefabScripts/Triggers/TriggerInterface.cs
+++ Assets/Scripts/PrefabScripts/Triggers/TriggerInterface.cs
@@ -5,7 +5,18 @@ namespace Interfaces
     public interface TriggerInterface
     {
         void setupEventObject();
-
+        // Method body must be:
+        // {
+        //     // Fetch the correct event script
+        //     try
+        //     {
+        //         eventScript = eventObject.GetComponent(typeof(EventInterface)) as EventInterface;
+        //     }
+        //     catch (System.Exception e)
+        //     {
+        //         Debug.Log("You haven't connected a gameobject to this trigger" + e);
+        //     }
+        // }
 
     }
 }
