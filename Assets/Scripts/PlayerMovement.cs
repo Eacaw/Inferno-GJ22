5a59f1a7884876cc685264224ccc8e0cd1d3a420
Eacaw <davidpinchen@gmail.com>
2022-05-22 19:12:13 +0100

    Add keybing (E) to leave a corpse and respawn

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     // Base user speed
     [SerializeField] private float speed = 5.0f;
     private Rigidbody rb;
+    public GameObject PlayerCorpsePrefab;
+    private Vector3 spawnPoint;
 
     public int MaxJump = 1;
     int JumpCount = 0;
 
     void Start()
     {
+        spawnPoint = transform.position;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,6 +26,14 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && JumpCount < MaxJump)
         {
             Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 spawnAbove = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            GameObject capsule = Instantiate(PlayerCorpsePrefab, transform.position, transform.rotation);
+            transform.position = spawnPoint;
+            rb.velocity = Vector3.zero;
         }
 
         transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
