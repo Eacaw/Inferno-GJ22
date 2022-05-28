c617649a9d1715864bfc2f3e6bd38348b073023a
Eacaw <davidpinchen@gmail.com>
2022-05-28 17:40:16 +0100

    Bouncy boxes

--- Assets/Scripts/PrefabScripts/Events/LaserScript.cs
+++ Assets/Scripts/PrefabScripts/Events/LaserScript.cs
@@ -9,32 +9,28 @@ public class LaserScript : MonoBehaviour, EventInterface
     public GameObject laserBars;
     private PlayerMovement playerMovement;
     public AudioSource laserBuzzSound;
-
     public Light flickerLight;
-
+    public bool bounceCube = false;
     private GameObject deathTrigger;
 
     void Start()
     {
         playerMovement = playerInstance.GetComponent<PlayerMovement>();
         deathTrigger = GetComponent<BoxCollider>().gameObject;
-
     }
     public void executeEvent()
     {
         laserBuzzSound.Stop();
-
         flickerLight.enabled = false;
-        laserBars.SetActive(false);
+        laserBars.gameObject.SetActive(false);
         deathTrigger.SetActive(false);
     }
 
     public void endExecution()
     {
-
         laserBuzzSound.Play();
         flickerLight.enabled = true;
-        laserBars.SetActive(true);
+        laserBars.gameObject.SetActive(true);
         deathTrigger.SetActive(true);
     }
 
@@ -50,5 +46,18 @@ public class LaserScript : MonoBehaviour, EventInterface
         {
             Destroy(other.gameObject);
         }
+        // This is not a perfect solution, we need to refine this, it's going in the fixit bucket
+        else if (
+            other.gameObject.tag == "MovableTrigger")
+        {
+            if (this.bounceCube)
+            {
+                Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
+                // push the object backwards
+                otherRb.AddForce(-otherRb.velocity * 3.0f, ForceMode.Impulse);
+            }
+        }
+
+
     }
 }
